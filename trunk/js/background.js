03cb80826b66a80d39f09081824a92067e0510ec
hetao@hetao.name <hetao@hetao.name@fdef4c80-fe23-a6aa-b224-978788050843>
2010-06-02 11:02:16 +0000

    fix 保存记录有可能不对（分享时也会加数）

--- trunk/js/background.js
+++ trunk/js/background.js
@@ -1,6 +1,7 @@
 chrome.tabs.onUpdated.addListener(function(tabId,changeInfo,tab) {
 	if(changeInfo.status=="complete"){
-		if(tab.url.indexOf("v.youku.com/v_")>0){
+		var url = parseURL(tab.url);
+		if(url.host =="v.youku.com" && url.path.indexOf("/v_")!==false){
 			playlist.add(tab.url,tab.title);
 		}
 	}
@@ -17,6 +18,137 @@ function check(){
 }
 check();
 setInterval("check()",60000);
+
+function parseURL(buffer) {
+  var result = { };
+  result.protocol = "";
+  result.user = "";
+  result.password = "";
+  result.host = "";
+  result.port = "";
+  result.path = "";
+  result.query = "";
+
+  var section = "PROTOCOL";
+  var start = 0;
+  var wasSlash = false;
+
+  while(start < buffer.length) {
+    if(section == "PROTOCOL") {
+      if(buffer.charAt(start) == ':') {
+        section = "AFTER_PROTOCOL";
+        start++;
+      } else if(buffer.charAt(start) == '/' && result.protocol.length() == 0) { 
+        section = PATH;
+      } else {
+        result.protocol += buffer.charAt(start++);
+      }
+    } else if(section == "AFTER_PROTOCOL") {
+      if(buffer.charAt(start) == '/') {
+    if(!wasSlash) {
+          wasSlash = true;
+    } else {
+          wasSlash = false;
+          section = "USER";
+    }
+        start ++;
+      } else {
+        throw new ParseException("Protocol shell be separated with 2 slashes");
+      }       
+    } else if(section == "USER") {
+      if(buffer.charAt(start) == '/') {
+        result.host = result.user;
+        result.user = "";
+        section = "PATH";
+      } else if(buffer.charAt(start) == '?') {
+        result.host = result.user;
+        result.user = "";
+        section = "QUERY";
+        start++;
+      } else if(buffer.charAt(start) == ':') {
+        section = "PASSWORD";
+        start++;
+      } else if(buffer.charAt(start) == '@') {
+        section = '<strong class="highlight">HOST</strong>';
+        start++;
+      } else {
+        result.user += buffer.charAt(start++);
+      }
+    } else if(section == "PASSWORD") {
+      if(buffer.charAt(start) == '/') {
+        result.host = result.user;
+        result.port = result.password;
+        result.user = "";
+        result.password = "";
+        section = "PATH";
+      } else if(buffer.charAt(start) == '?') {
+        result.host = result.user;
+        result.port = result.password;
+        result.user = "";
+        result.password = "";
+        section = "QUERY";
+        start ++;
+      } else if(buffer.charAt(start) == '@') {
+        section = '<strong class="highlight">HOST</strong>';
+        start++;
+      } else {
+        result.password += buffer.charAt(start++);
+      }
+    } else if(section == '<strong class="highlight">HOST</strong>') {
+      if(buffer.charAt(start) == '/') {
+        section = "PATH";
+      } else if(buffer.charAt(start) == ':') {
+        section = "PORT";
+        start++;
+      } else if(buffer.charAt(start) == '?') {
+        section = "QUERY";
+        start++;
+      } else {
+        result.host += buffer.charAt(start++);
+      }
+    } else if(section == "PORT") {
+      if(buffer.charAt(start) = '/') {
+        section = "PATH";
+      } else if(buffer.charAt(start) == '?') {
+        section = "QUERY";
+        start++;
+      } else {
+        result.port += buffer.charAt(start++);
+      }
+    } else if(section == "PATH") {
+      if(buffer.charAt(start) == '?') {
+    section = "QUERY";
+    start ++;
+      } else {
+    result.path += buffer.charAt(start++);
+      }
+    } else if(section == "QUERY") {
+      result.query += buffer.charAt(start++);
+    }
+  }
+
+  if(section == "PROTOCOL") {
+    result.host = result.protocol;
+    result.protocol = "http";
+  } else if(section == "AFTER_PROTOCOL") {
+    throw new ParseException('Invalid <strong class="highlight">url</strong>');
+  } else if(section == "USER") {
+    result.host = result.user;
+    result.user = "";
+  } else if(section == "PASSWORD") {
+    result.host = result.user;
+    result.port = result.password;
+    result.user = "";
+    result.password = "";
+  }
+
+  return result;
+}
+
+function ParseException(description) {
+    this.description = description;
+}
+
 /*
 chrome.browserAction.onClicked.addListener(function(tab) {
 		chrome.browserAction.setBadgeText({text:""});
