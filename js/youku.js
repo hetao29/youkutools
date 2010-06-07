a21646f2cfaad9a73c9ad5021fb0215ab16639e7
hetao@hetao.name <hetao@hetao.name@fdef4c80-fe23-a6aa-b224-978788050843>
2010-06-07 10:28:32 +0000

    0.56 增加视频循环播放功能

--- js/youku.js
+++ js/youku.js
@@ -1,6 +1,9 @@
-﻿/*
+﻿function youku_chrome_autoplay(){
+	onPlayerComplete=function(){
+		if($("replay").checked)PlayerSeek(0);
+	}
+}
 $("#movie_player").ready(function(){
-	$(".resize").append('<span class=""><a charset="400-02-6" onclick="'+"alert($('player').style.width);$('player').style.width=$('player').style.width+5"+'">+ / -</a></span>');
-
+	$(".resize").append('<span class="break">|</span> <span><input id="replay" style=" margin-bottom:1px" type="checkbox"/> <a href="javascript:$(\'replay\').checked?$(\'replay\').checked=false:$(\'replay\').checked=true">循环播放</a></label></a></span>');
+	$(".resize").append("<script>"+youku_chrome_autoplay+";youku_chrome_autoplay();</script>");
 });
-*/
