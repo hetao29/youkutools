a6b7eb460fe2786606210cd8da72d404fae8fccb
hetao@hetao.name <hetao@hetao.name@fdef4c80-fe23-a6aa-b224-978788050843>
2010-12-17 10:53:45 +0000

    0.62 patch

--- trunk/js/youku.js
+++ trunk/js/youku.js
@@ -21,7 +21,9 @@ $("#movie_player").ready(function(){
 	$(".resize").append('<span class="break">|</span> <span><input id="replay" style=" margin-bottom:1px" type="checkbox"/> <a href="javascript:$(\'replay\').checked?$(\'replay\').checked=false:$(\'replay\').checked=true">循环播放</a></label></a></span>');
 	$(".resize").append("<script>"+youku_chrome_autoplay+";youku_chrome_autoplay();</script>");
 	$(".resize").append("<script>"+youku_chrome_autoplay+";youku_chrome_autoplay();</script>");
-	if(!$(".crumbs A:contains('音乐')").html())return;;
+	if(	
+		!$(".crumbs A:contains('音乐')").html() && $("#vpofficialtitle")==null
+	)return;;
 	$.ajax({
 		url: url+"player.main.getlyric",
 		data: {
